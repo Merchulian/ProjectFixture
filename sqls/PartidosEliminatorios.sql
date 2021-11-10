158af19d47509bc4eda71cff153df3a967916e42
Martin Freire <martin.m.freire@gmail.com>
2021-11-09 21:07:17 -0300

    añadidos los scripts sql

---
+++ sqls/PartidosEliminatorios.sql
@@ -0,0 +1,17 @@
+/*partidos Eliminatorios*/
+
+/*Cuartos*/
+insert INTO calendario_eliminatorio values (1 ,"Cuartos",'2021-11-29',null , null);
+insert INTO calendario_eliminatorio values (2 ,"Cuartos",'2021-11-29',null,null);
+insert INTO calendario_eliminatorio values (3,"Cuartos",'2021-11-29' ,null,null);
+insert INTO calendario_eliminatorio values (4 ,"Cuartos",'2021-11-29',null,null);
+
+/*Semifinales*/
+insert INTO calendario_eliminatorio values (5 ,"Semifinales",'2021-11-30',null, null);
+insert INTO calendario_eliminatorio values (6 ,"Semifinales",'2021-11-30' ,null, null);
+
+/*3er y 4to Puesto*/
+insert INTO calendario_eliminatorio values (7 ,"3ER Y 4TO Puesto",'2021-12-1',null,null);
+
+/*Final*/
+insert INTO calendario_eliminatorio values (8,"Final",'2021-12-1',null,null);
