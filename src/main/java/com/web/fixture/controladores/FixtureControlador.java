cd09affa9875f39830b1bd3cb01b7bf8ce03b5dd
Martin Freire <martin.m.freire@gmail.com>
2021-11-15 20:53:49 -0300

    agregando postMappings en el fixture controlador

--- src/main/java/com/web/fixture/controladores/FixtureControlador.java
+++ src/main/java/com/web/fixture/controladores/FixtureControlador.java
@@ -6,21 +6,44 @@
 package com.web.fixture.controladores;
 
 import com.web.fixture.entidades.Equipo;
+import com.web.fixture.entidades.Fixture;
+import com.web.fixture.entidades.PartidoEliminatorio;
+import com.web.fixture.entidades.PartidoGrupo;
+import com.web.fixture.entidades.Usuario;
+import com.web.fixture.errores.ErrorServicio;
 import com.web.fixture.servicios.FixtureServicio;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/fixture")
 public class FixtureControlador {
-    
+    @Autowired
     private FixtureServicio fixtureServicio;
     
     @GetMapping("/fixture")
-    public String fixture() {
-
+    @PreAuthorize("hasAnyRole('ROLE_USUARIO_REGISTRADO')")
+    public String fixture( ModelMap model, HttpSession session) throws ErrorServicio{
+        //recupero la info del usuario logueado
+        Usuario login = (Usuario) session.getAttribute("usuariosession");
+        // recurro al Fixture del usuario para mostrar en pantalla la info 
+        //que ya haya ingresado
+        Fixture fixture = login.getFixture();
+        List<PartidoGrupo> listaGrupo = new ArrayList();
+        List<PartidoEliminatorio> listaEliminatorio = new ArrayList();
+        // mostrar fase de grupos
+        //armar fase eliminatorias
+        //mostrar fase eliminatoria
         return "fixture.html";
+ 
     }
     
     /* MÉTODO GANADOR VA A UTILIZAR UNA QUERY DE EQUIPOREP para buscar el equipo que tiene como fase "ganador". 
@@ -38,6 +61,23 @@ public class FixtureControlador {
 
         }
         return null;
+    }
+    
+    @PostMapping("/definir-grupo-A")
+    public String definirA(){
+    return "redirect:/fixture";
+    }
+        @PostMapping("/definir-grupo-B")
+    public String definirB(){
+    return "redirect:/fixture";
+    }
+        @PostMapping("/definir-grupo-C")
+    public String definirC(){
+    return "redirect:/fixture";
+    }
+        @PostMapping("/definir-grupo-D")
+    public String definirD(){
+    return "redirect:/fixture";
     }
     
        
